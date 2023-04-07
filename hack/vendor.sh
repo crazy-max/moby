9ab42e8cf395c0a53cacd99c854f88b436daab30
CrazyMax <crazy-max@users.noreply.github.com>
2023-04-07 08:28:19 +0200

    hack: simplify vendor validation
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -18,10 +18,20 @@ vendor() (
 		"${SCRIPTDIR}"/with-go-mod.sh go mod vendor -modfile vendor.mod
 )
 
+validate() (
+		diff=$(git status --porcelain -- vendor.mod vendor.sum vendor)
+		if [ -n "$diff" ]; then
+			echo >&2 'ERROR: Vendor result differs. Please revendor with hack/vendor.sh'
+			echo "$diff"
+			exit 1
+		fi
+)
+
 help() {
 	printf "%s:\n" "$(basename "$0")"
 	echo "  - tidy: run go mod tidy"
 	echo "  - vendor: run go mod vendor"
+	echo "  - validate: validate vendor"
 	echo "  - all: run tidy && vendor"
 	echo "  - help: show this help"
 }
@@ -29,6 +39,7 @@ help() {
 case "$1" in
 	tidy) tidy ;;
 	vendor) vendor ;;
+	validate) validate ;;
 	""|all) tidy && vendor ;;
 	*) help ;;
 esac
