424c95b3795c9f63311ced156951ee1499ad2e42
Tonis Tiigi <tonistiigi@gmail.com>
2023-05-15 19:53:58 -0700

    client: fix leaking idle connections
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    
    # Conflicts:
    #	client/client.go

--- client/client.go
+++ client/client.go
@@ -48,6 +48,7 @@ import (
 	"net/url"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/docker/docker/api"
 	"github.com/docker/docker/api/types"
@@ -172,6 +173,9 @@ func defaultHTTPClient(hostURL *url.URL) (*http.Client, error) {
 	if err != nil {
 		return nil, err
 	}
+	transport.MaxIdleConns = 6
+	transport.IdleConnTimeout = 30 * time.Second
+	// transport.DisableKeepAlives = true
 	return &http.Client{
 		Transport:     transport,
 		CheckRedirect: CheckRedirect,
